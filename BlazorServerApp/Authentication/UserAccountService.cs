1a6f730b52caa1f966390570330f5b1a817e8045
Paulino Auyon <pauyon@jgboswell.com>
2024-04-01 15:14:38 -0700

    Initial Commit

---
+++ BlazorServerApp/Authentication/UserAccountService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BlazorServerApp.Authentication
+{
+    public class UserAccountService
+    {
+        private List<UserAccount> _users = new();
+
+        public UserAccountService()
+        {
+            _users = new List<UserAccount>
+            {
+                new UserAccount { UserName = "admin", Password = "admin", Role = "Administrator" },
+                new UserAccount { UserName = "user", Password = "user", Role = "User" },
+            };
+        }
+
+        public UserAccount? GetByUsername(string userName)
+        {
+            return _users.FirstOrDefault(x => x.UserName == userName);
+        }
+    }
+}
